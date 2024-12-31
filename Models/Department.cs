c21d54c7e3cc812e5401077eca834ee63678f677
zeyadtowork <zeyadtowork@gmial.com>
2024-12-31 06:04:40 +0200

    edit

--- Models/Department.cs
+++ Models/Department.cs
@@ -6,8 +6,5 @@
         public string name { get; set; }
         public string description { get; set; }
         public string head_of_department { get; set; }
-        // zeyad ragy
-
-        // mahmoud
     }
 }
